6e3450e6bd38f7a46de95ecb24bff958c5ad309d
pashaadelia <pasha.pat06@gmail.com>
2026-08-18 17:14:33 +0700

    features/delete-register

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ Route::get('/', function () {
 // Auth
 Route::get('/login/{role}', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
 Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
