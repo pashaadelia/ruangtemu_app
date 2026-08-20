c36470760c54e81ec775c6d9ea39a4fc9eb21832
tntrindh <tantriindaho26@gmail.com>
2026-08-20 09:02:16 +0700

    tan, features/create-setting-page

--- resources/views/admin/pengaturan.blade.php
+++ resources/views/admin/pengaturan.blade.php
@@ -77,7 +77,7 @@
                                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/>
                                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 0 1 8-8v4a4 4 0 0 0-4 4H4z"/>
                             </svg>
-                            <span x-text="saving ? 'Menyimpan...' : 'Simpan Perubahan'"></span>
+                            <span x-text="saving ? 'Menyimpan...' : 'Simpan Perubahan'">Simpan Perubahan</span>
                         </button>
                     </div>
                 </form>
@@ -179,7 +179,7 @@
                                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/>
                                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 0 1 8-8v4a4 4 0 0 0-4 4H4z"/>
                             </svg>
-                            <span x-text="updating ? 'Memperbarui...' : 'Update Keamanan'"></span>
+                            <span x-text="updating ? 'Memperbarui...' : 'Update Keamanan'">Update Keamanan</span>
                         </button>
                     </div>
                 </form>
